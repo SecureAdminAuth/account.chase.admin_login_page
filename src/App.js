9c078affcdba66ce40bc91212090671b43715630
SecureAdminAuth <webauth20@gmail.com>
2024-09-30 02:23:41 +0530

    add 2nd account

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import LoginPage from "./Components/LoginPage";
 import Home from "./Components/Home";
 // import Account_Page from "./Components/Account_Page";
 import Acc_Page_Frame from "./Components/Acc_Page_Frame";
+import Acc_Page_Frame2 from "./Components/Acc_Page_Frame2";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/*" element={<Home />} />
           <Route path="/account" element={<Acc_Page_Frame />} />
+          <Route path="/account2" element={<Acc_Page_Frame2 />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/Components/Acc_Page_Frame2.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Acc_Page_Frame2 = () => {
+  return (
+    <div>
+      <iframe
+        src="https://loginpage2-nib6.onrender.com"
+        title="Account Details Page"
+        style={{ width: "100%", height: "100vh", border: "none" }}
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default Acc_Page_Frame2;
